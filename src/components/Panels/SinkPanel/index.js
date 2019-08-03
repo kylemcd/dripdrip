a2f2cc6162bd7d75f11291d4cdad2e28b45eaddf
Kyle McDonald <hello@kylemcd.com>
2019-08-03 11:00:32 -0500

    Adds sink panel usage

--- src/components/Panels/SinkPanel/index.js
+++ src/components/Panels/SinkPanel/index.js
@@ -1,13 +1,37 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
+
+import {
+  Timer
+} from '../../../components';
+
+import {
+  WaterContext
+} from '../../../context';
 
 class SinkPanel extends Component {
+
+  subtractFromTotal = (time) => {
+    const { updateWaterAmount } = this.context;
+
+    const gallonsPerSecond = 0.036;
+
+    const waterUsage = time * gallonsPerSecond;
+
+    updateWaterAmount(waterUsage);
+  }
+
   render() {
     return (
-      <div>
-        SinkPanel
-      </div>
+      <Fragment>
+        <Timer
+          startTime={400}
+          onSubtract={this.subtractFromTotal.bind(this)}
+        />
+      </Fragment>
     );
   }
 }
+
+SinkPanel.contextType = WaterContext;
 
 export default SinkPanel;
